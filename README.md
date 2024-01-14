b9d1f7484472f35d61405bcbfd73fb1c045f9da7
ehsansdr <sehsan251@gmail.com>
2024-01-14 12:01:02 +0330

    README.md done

---
+++ README.md
@@ -0,0 +1,25 @@
+**Ping Pong game**
+
+* 120 fbs and more
+* sound effect
+* special game loop
+* commented code
+* color detail
+* fair game in spawning and detailing 
+
+![img_1.png](img_1.png)
+
+
+
+
+
+![img_2.png](img_2.png)
+
+
+
+
+
+![img_3.png](img_3.png)
+
+
+
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,13 +6,14 @@ public class GameFrame extends JFrame {
     GamePanel gamePanel;
     public Paddle paddleR ;
     public Paddle paddleL;
-
+    ImageIcon FrameGameIcon = new ImageIcon("FrameGameIcon.png");
 
 
     public GameFrame(GamePanel gamePanel){
         this.gamePanel = gamePanel;
 
         this.setTitle("ping pong game");
+        this.setIconImage(FrameGameIcon.getImage());
         this.add(this.gamePanel);
 
         this.setResizable(true);
